cef80588ecd4e1867b206de8f77a2ed72b81b30c
Muhammed Bilal A <bilal.muhammed@finbox.in>
2023-05-07 01:40:37 +0530

    configs

---
+++ nvim_custom/configs/mason.lua
@@ -0,0 +1,16 @@
+return {
+      ensure_installed = {
+        -- lua stuff
+        "typescript-language-server",
+        "gopls",
+"bash-language-server",
+      "black",
+      "goimports",
+      "gopls",
+      "python-lsp-server",
+      "rust-analyzer",
+      "rustfmt",
+      "vim-language-server",
+      "yaml-language-server",
+      }
+    }
---
+++ nvim_custom/configs/options.lua
@@ -0,0 +1,7 @@
+vim.api.nvim_set_option('relativenumber', true)
+vim.api.nvim_set_option('updatetime', 900)
+vim.api.nvim_set_option('showcmd', false)
+vim.api.nvim_set_option('cmdheight', 0)
+-- Neovide settings
+vim.g.neovide_refresh_rate = 60
+vim.g.neovide_cursor_antialiasing = true
