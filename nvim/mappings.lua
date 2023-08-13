02eb13515c9dd74a1f1796e2c88720a63ecae055
Muhammed Bilal A <bilal.muhammed@finbox.in>
2023-08-13 17:43:22 +0530

    FIX: Update nvchad module name

--- nvim/mappings.lua
+++ nvim/mappings.lua
@@ -84,14 +84,14 @@ M.tabufline = {
     -- cycle through buffers
     ["<TAB>"] = {
       function()
-        require("nvchad_ui.tabufline").tabuflineNext()
+        require("nvchad.tabufline").tabuflineNext()
       end,
       "goto next buffer",
     },
 
     ["<S-Tab>"] = {
       function()
-        require("nvchad_ui.tabufline").tabuflinePrev()
+        require("nvchad.tabufline").tabuflinePrev()
       end,
       "goto prev buffer",
     },
@@ -102,7 +102,7 @@ M.tabufline = {
     -- close buffer + hide terminal buffer
     ["<leader>bd"] = {
       function()
-        require("nvchad_ui.tabufline").close_buffer()
+        require("nvchad.tabufline").close_buffer()
       end,
       "close buffer",
     },
@@ -180,7 +180,7 @@ M.lspconfig = {
 
     ["<leader>ra"] = {
       function()
-        require("nvchad_ui.renamer").open()
+        require("nvchad.renamer").open()
       end,
       "lsp rename",
     },
