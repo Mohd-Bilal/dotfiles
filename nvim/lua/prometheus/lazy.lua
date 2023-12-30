fab9fbb03a9960dc24e1076d31b1b866a8167e93
Bilal <bilaljaleel123@gmail.com>
2023-12-31 03:43:13 +0530

    Setup markdown preview

--- nvim/lua/prometheus/lazy.lua
+++ nvim/lua/prometheus/lazy.lua
@@ -3,118 +3,127 @@
 -- Only required if you have packer configured as `opt`
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+   vim.fn.system({
+      "git",
+      "clone",
+      "--filter=blob:none",
+      "https://github.com/folke/lazy.nvim.git",
+      "--branch=stable", -- latest stable release
+      lazypath,
+   })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  {
-    'nvim-telescope/telescope.nvim',
-    dependencies = { { 'nvim-lua/plenary.nvim' } },
-    config = function()
-      require("prometheus.config.telescope")
-    end
-  },
-  {
-    'rose-pine/neovim',
-    as = 'rose-pine',
-    config = function()
-      vim.cmd('colorscheme rose-pine')
-    end
-  },
-  {
-    'nvim-treesitter/nvim-treesitter',
-    config = function()
-      require("prometheus.config.treesitter")
-    end
-  },
-  { "nvim-treesitter/nvim-treesitter-context" },
-  {
-    'VonHeikemen/lsp-zero.nvim',
-    dependencies = {
-      -- LSP Support
-      { 'neovim/nvim-lspconfig' },
-      { 'williamboman/mason.nvim' },
-      { 'williamboman/mason-lspconfig.nvim' },
+   {
+      'nvim-telescope/telescope.nvim',
+      dependencies = { { 'nvim-lua/plenary.nvim' } },
+      config = function()
+         require("prometheus.config.telescope")
+      end
+   },
+   {
+      'rose-pine/neovim',
+      as = 'rose-pine',
+      config = function()
+         vim.cmd('colorscheme rose-pine')
+      end
+   },
+   {
+      'nvim-treesitter/nvim-treesitter',
+      config = function()
+         require("prometheus.config.treesitter")
+      end
+   },
+   { "nvim-treesitter/nvim-treesitter-context" },
+   {
+      'VonHeikemen/lsp-zero.nvim',
+      dependencies = {
+         -- LSP Support
+         { 'neovim/nvim-lspconfig' },
+         { 'williamboman/mason.nvim' },
+         { 'williamboman/mason-lspconfig.nvim' },
 
-      -- Autocompletion
-      { 'hrsh7th/nvim-cmp' },
-      { 'hrsh7th/cmp-buffer' },
-      { 'hrsh7th/cmp-path' },
-      { 'saadparwaiz1/cmp_luasnip' },
-      { 'hrsh7th/cmp-nvim-lsp' },
-      { 'hrsh7th/cmp-nvim-lua' },
+         -- Autocompletion
+         { 'hrsh7th/nvim-cmp' },
+         { 'hrsh7th/cmp-buffer' },
+         { 'hrsh7th/cmp-path' },
+         { 'saadparwaiz1/cmp_luasnip' },
+         { 'hrsh7th/cmp-nvim-lsp' },
+         { 'hrsh7th/cmp-nvim-lua' },
 
-      -- Snippets
-      {
-        'L3MON4D3/LuaSnip',
-        build = "make install_jsregexp"
+         -- Snippets
+         {
+            'L3MON4D3/LuaSnip',
+            build = "make install_jsregexp"
+         },
+         { 'rafamadriz/friendly-snippets' },
       },
-      { 'rafamadriz/friendly-snippets' },
-    },
-    config = function()
-      require("prometheus.config.lsp")
-      require("prometheus.config.cmp")
-    end
-  },
-  {
-    "laytan/cloak.nvim",
-    config = function()
-      require("prometheus.config.cloak")
-    end
-  },
-  {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("prometheus.config.nvim-tree")
-    end
-  },
-  {
-    "willothy/nvim-cokeline",
-    dependencies = {
-      "nvim-lua/plenary.nvim",        -- Required for v0.4.0+
-      "kyazdani42/nvim-web-devicons", -- If you want devicons
-      "stevearc/resession.nvim"       -- Optional, for persistent history
-    },
-    config = function()
-      require("prometheus.config.tabline")
-    end
-  },
-  {
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    opts = {} -- this is equalent to setup({}) function
-  },
-  {
-    "onsails/lspkind.nvim"
-  },
-  {
-    "themercorp/themer.lua",
-    config = function()
-      require("prometheus.config.themer")
-    end
-  },
-  {
-    'numToStr/Comment.nvim',
-    event = "BufEnter",
-    config = function()
-      require('Comment').setup()
-    end,
-  },
-  {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      require("prometheus.config.statusline")
-    end
-  }
+      config = function()
+         require("prometheus.config.lsp")
+         require("prometheus.config.cmp")
+      end
+   },
+   {
+      "laytan/cloak.nvim",
+      config = function()
+         require("prometheus.config.cloak")
+      end
+   },
+   {
+      "nvim-tree/nvim-tree.lua",
+      dependencies = { "nvim-tree/nvim-web-devicons" },
+      config = function()
+         require("prometheus.config.nvim-tree")
+      end
+   },
+   {
+      "willothy/nvim-cokeline",
+      dependencies = {
+         "nvim-lua/plenary.nvim",        -- Required for v0.4.0+
+         "kyazdani42/nvim-web-devicons", -- If you want devicons
+         "stevearc/resession.nvim"       -- Optional, for persistent history
+      },
+      config = function()
+         require("prometheus.config.tabline")
+      end
+   },
+   {
+      'windwp/nvim-autopairs',
+      event = "InsertEnter",
+      opts = {} -- this is equalent to setup({}) function
+   },
+   {
+      "onsails/lspkind.nvim"
+   },
+   {
+      "themercorp/themer.lua",
+      config = function()
+         require("prometheus.config.themer")
+      end
+   },
+   {
+      'numToStr/Comment.nvim',
+      event = "BufEnter",
+      config = function()
+         require('Comment').setup()
+      end,
+   },
+   {
+      'nvim-lualine/lualine.nvim',
+      dependencies = { 'nvim-tree/nvim-web-devicons' },
+      config = function()
+         require("prometheus.config.statusline")
+      end
+   },
+   {
+      "iamcco/markdown-preview.nvim",
+      cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+      build = "cd app && npm install",
+      init = function()
+         vim.g.mkdp_filetypes = { "markdown" }
+      end,
+      ft = { "markdown" },
+   },
 
 })
