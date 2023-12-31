66fc877820f6a4cc45afbde2ce4ef5f47fc3f298
Bilal <bilaljaleel123@gmail.com>
2024-01-01 03:35:38 +0530

    Refactor

--- nvim/lua/prometheus/autocmds.lua
+++ nvim/lua/prometheus/autocmds.lua
@@ -1,7 +1,7 @@
 local augroup = vim.api.nvim_create_augroup
-local prometheus_group = augroup('prometheus', {})
-
 local autocmd = vim.api.nvim_create_autocmd
+
+local prometheus_group = augroup('prometheus', {})
 local yank_group = augroup('HighlightYank', {})
 local theme_group = augroup('ThemeChange', {})
 
