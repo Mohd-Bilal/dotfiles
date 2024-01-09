905b1d77e9e823da2c2abf168187558f4ccda140
Muhammed Bilal A <bilal.muhammed@finbox.in>
2024-01-10 00:28:28 +0530

    [FEAT]: Add cmp config (none)

--- nvim/lua/prometheus/config/cmp.lua
+++ nvim/lua/prometheus/config/cmp.lua
@@ -1,66 +1,32 @@
 local cmp = require('cmp')
-local cmp_action = require('lsp-zero.cmp').action()
-
-require('luasnip.loaders.from_vscode').lazy_load()
-
-vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
-
+local cmp_format = require('lsp-zero').cmp_format()
+local cmp_action = require('lsp-zero').cmp_action()
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 
-
 cmp.setup({
-    preselect = 'item',
-    completion = {
-        completeopt = 'menu,menuone,noinsert'
-    },
-    window = {
-        completion = {
-            winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
-            col_offset = -3,
-            side_padding = 0,
-        },
-    },
-
-    formatting = {
-        fields = { "kind", "abbr", "menu" },
-        format = function(entry, vim_item)
-            vim_item.menu = ({
-                buffer = "[Buffer]",
-                nvim_lsp = "[LSP]",
-                luasnip = "[LuaSnip]",
-                nvim_lua = "[Lua]",
-                latex_symbols = "[LaTeX]",
-            })[entry.source.name]
-            local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 100 })(entry, vim_item)
-            local strings = vim.split(kind.kind, "%s", { trimempty = true })
-            kind.kind = " " .. (strings[1] or "") .. " "
-            kind.menu = "    (" .. (strings[2] or "") .. ")"
-
-            return kind
-        end,
-    },
-    mapping = {
-        -- confirm completion item
-        ['<CR>'] = cmp.mapping.confirm({ select = false }),
-
-        -- toggle completion menu
-        ['<C-e>'] = cmp_action.toggle_completion(),
-
-        -- tab complete
-        ['<Tab>'] = cmp_action.tab_complete(),
-        ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-
-        -- navigate between snippet placeholder
-        ['<C-d>'] = cmp_action.luasnip_jump_forward(),
-        ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-
-        -- scroll documention window
-        ['<C-f>'] = cmp.mapping.scroll_docs(5),
-        ['<C-u>'] = cmp.mapping.scroll_docs(-5),
-    },
+  enabled = true,
+  preselect = cmp.PreselectMode.None,
+  completion = {
+    completeopt = 'menu,menuone,noinsert'
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'buffer' },
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<Tab>'] = cmp_action.luasnip_supertab(),
+    ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+    ['<CR>'] = cmp.mapping.confirm({ select = false }),
+  }),
+  --- (Optional) Show source name in completion menu
+  formatting = cmp_format,
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  }
 })
 
 cmp.event:on(
-    'confirm_done',
-    cmp_autopairs.on_confirm_done()
+  'confirm_done',
+  cmp_autopairs.on_confirm_done()
 )
